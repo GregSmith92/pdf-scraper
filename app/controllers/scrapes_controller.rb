a7d39e82cc560526a72cc0c99aa96aee7400a202
Greg Smith <greg_smith1992@hotmail.com>
2019-06-16 16:18:35 +0100

    Added some methods to the scrapes controller

--- app/controllers/scrapes_controller.rb
+++ app/controllers/scrapes_controller.rb
@@ -1,2 +1,30 @@
+require 'open-uri'
 class ScrapesController < ApplicationController
+
+  def new
+    @scrape = Scrape.new
+  end
+
+  def create
+    @scrape = Scrape.new(scrape_params)
+    if @scrape.save
+      # io     = open("#{@scrape.url}")
+      io = open(@scrape.url)
+      reader = PDF::Reader.new(io)
+      puts reader.info
+  end
+
+  def show
+
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def scrape_params
+    params.require(:scrape).permit(:url)
+  end
 end
